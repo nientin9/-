fe658a607b089badd5f3225d5168045e6deb4bf2
nientin9 <80729717+nientin9@users.noreply.github.com>
2021-03-21 15:04:57 +0800

    Update 抽抽樂.py

--- 抽抽樂.py
+++ 抽抽樂.py
@@ -38,7 +38,7 @@ driver.get("https://fuli.gamer.com.tw")
 driver.maximize_window()
 
 # selenium add cookies(帳密)
-rawdata = "add cookies"
+rawdata = 'add cookies'
 cookie = SimpleCookie()
 cookie.load(rawdata)
 cookies = {}
@@ -48,18 +48,18 @@ for key, morsel in cookie.items():
     driver.add_cookie({"name": key, "value": morsel.value})
 
 times = 0  # selenium 執行次數
-for times in range(10):
+for times in range(2):
     for lucky_draw_link in lucky_draw_links:
         driver.get(lucky_draw_link)  # get(抽抽樂連結)
         driver.find_element_by_class_name("c-accent-o").click()  # 看廣告免費兌換
-    
+
         wait = ui.WebDriverWait(driver, 10)  # Python+Selenium定位不到元素常見原因及解決辦法(https://www.cnblogs.com/awakenedy/articles/9778634.html)
         # question-popup
         question_popup_element_exist = True if len(driver.find_elements_by_id("answer-count")) > 0 else False  # 利用"共需答對幾題?"判斷是否彈跳出勇者問答題
         if question_popup_element_exist == True:
             answer_count_split = driver.find_element_by_id("answer-count").text.split()
             answer_count = int(answer_count_split[1])  # 取題數
-    
+
             questions = 0  # 執行問題(次數)
             element_id = 1
             for questions in range(answer_count):
@@ -72,12 +72,12 @@ for times in range(10):
                 time.sleep(3)  # wait for the webpage to load before executing the next line of code (https://stackoverflow.com/questions/60824679/time-sleep-on-chromedriver)
                 element_id += 1
                 questions += 1
-    
+
             # watch_ad
             watch_ad_element = wait.until(lambda driver: driver.find_element_by_id("btn-buy"))
             driver.execute_script("arguments[0].click();", watch_ad_element)  # there is another element (div below the button) will receive the click. Driver.execute_script("arguments[0].click();", element) Takes your locator (element) as first argument and perform the action of click.(https://sqa.stackexchange.com/questions/40678/using-python-selenium-not-able-to-perform-click-operation)
             time.sleep(3)
-    
+
         # if_watch_ad
         try:
             if_watch_ad = wait.until(lambda driver: driver.find_element_by_class_name("btn-primary"))
@@ -85,14 +85,7 @@ for times in range(10):
         except TimeoutException:
             print('if_watch_ad: TimeoutException(沒有彈跳出"是否觀看廣告?"視窗)')
             pass
-    
-        # 發生錯誤，請重新嘗試(1) 或  廣告能量補充中 請稍後再試
-        btn_danger_element_exist = True if len(driver.find_elements_by_class_name("btn-danger")) > 0 else False
-        if btn_danger_element_exist == True:
-            print("btn_danger_element_exist:", btn_danger_element_exist)
-            driver.find_element_by_class_name("btn-danger").click()
-            driver.quit()
-    
+
         # close_ad
         # print(len(driver.find_elements_by_tag_name('iframe')))
         iframe = wait.until(lambda driver: driver.find_elements_by_tag_name('iframe')[-1])  # python+selenium 自動化過程中遇到的元素不可見時間以及webelement不可見的處理方法(https://iter01.com/467737.html)
@@ -110,12 +103,12 @@ for times in range(10):
                     WebDriverWait(driver, 180).until(EC.invisibility_of_element_located((By.ID, "count_down")))
                     WebDriverWait(driver, 180).until(EC.element_to_be_clickable((By.ID, "close_button_icon"))).click()
                 break
-    
+
             dismiss_button_element_exist = True if len(driver.find_elements_by_id("dismiss-button-element")) > 0 else False
             if dismiss_button_element_exist == True:
                 WebDriverWait(driver, 180).until(EC.element_to_be_clickable((By.ID, "dismiss-button-element"))).click()
                 break
-    
+
             close_circle_element_exist = True if len(driver.find_elements_by_xpath('//*[@id="google-rewarded-video"]/img[3]')) > 0 else False
             if close_circle_element_exist == True:
                 print("close_circle_element_exist:", close_circle_element_exist)
@@ -123,16 +116,8 @@ for times in range(10):
                 time.sleep(3)  # 播放結束
                 driver.find_element_by_xpath('//*[@id="google-rewarded-video"]/img[3]').click()
                 break
-    
             break
 
-        # 發生錯誤，請重新嘗試(1) 或  廣告能量補充中 請稍後再試
-        btn_danger_element_exist = True if len(driver.find_elements_by_class_name("btn-danger")) > 0 else False
-        if btn_danger_element_exist == True:
-            print("btn_danger_element_exist:", btn_danger_element_exist)
-            driver.find_element_by_class_name("btn-danger").click()
-            driver.quit()
-    
         # agree_confirm
         driver.switch_to.default_content()
         time.sleep(3)  # 等待換頁
@@ -144,9 +129,9 @@ for times in range(10):
         time.sleep(3)  # 觀察打勾
         WebDriverWait(driver, 180).until(EC.element_to_be_clickable((By.CLASS_NAME, "c-primary"))).click()
         # driver.find_element_by_class_name("c-primary").click()  # 確定兌換
-        
+
         # 您尚未勾選「我已閱讀注意事項，並確認兌換此商品」
-        btn_danger_element_exist = True if len(driver.find_elements_by_class_name("btn-danger")) > 0 else False  
+        btn_danger_element_exist = True if len(driver.find_elements_by_class_name("btn-danger")) > 0 else False
         while btn_danger_element_exist == True:
             print("您尚未勾選「我已閱讀注意事項，並確認兌換此商品」")
             driver.find_element_by_class_name("btn-danger").click()
@@ -158,11 +143,8 @@ for times in range(10):
                 break
             else:
                 continue
-    
+
         submit = wait.until(lambda driver: driver.find_element_by_class_name("btn-primary"))  # 您確定要兌換此商品嗎？
         submit.click()
     times += 1
 driver.quit()
-
-
-
